c3be7b1b35d3dbaaf71e2e805427374586537471
Paul-Hil <famhilairep@gmail.com>
2021-08-21 22:37:11 +0200

    tesh hotfix mep

--- public/content/plugins/monpotager/class/Plugin.php
+++ public/content/plugins/monpotager/class/Plugin.php
@@ -25,20 +25,10 @@ class Plugin
     
         add_action('save_post', [$maSemi, 'save_metaboxes']);
         
-
         add_action('rest_api_init', [$this, 'api_meta']);
-
 
         add_action('add_meta_boxes', [$userPlanting, 'user_Metaboxes_Planting']);
         add_action('save_post', [$userPlanting, 'saveUserMetaboxesDaysPlantation']);
-
-        
-
-        
-
-
-
-        
     }
 
     /**
